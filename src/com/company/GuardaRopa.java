ed7ec91a45612bc3819912c5ccaa665bc2f2473d
Theo <theomusumeci@gmail.com>
2020-03-15 21:14:30 -0300

    Arranco el ejercicio

---
+++ src/com/company/GuardaRopa.java
@@ -0,0 +1,36 @@
+package com.company;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+
+public class GuardaRopa {
+
+private  Map<Integer, String> listaDePrendas = new HashMap<>();
+private Integer contador;
+
+    public Map<Integer, String> getListaDePrendas() {
+        return listaDePrendas;
+    }
+
+    public Integer getContador() {
+        return contador;
+    }
+
+    public GuardaRopa(Map<Integer, String> listaDePrendas) {
+        this.listaDePrendas = listaDePrendas;
+        this.contador = contador;
+    }
+
+    public Integer guardarPrendas(List<Prenda> listaDePrendas) {
+        for (Integer identificador: listaDePrendas.size()) {
+listaDePrendas.get(1);
+
+        }
+
+
+    }
+
+
+
+}
---
+++ src/com/company/Prenda.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Prenda {
+
+    public String marca;
+    public String modelo;
+
+
+}
